9afeb994659ebfe44b38f7d36fc3c230ee3cf937
Gabriel Marcano <gabemarcano@yahoo.com>
2024-06-04 15:08:54 -0700

    Update to work with latest asimple
     - asimple API has changed quite a bit since this was last updated
     - update linker script
     - update meson.build to use newer SDK naming

--- src/main.c
+++ src/main.c
@@ -1,39 +1,21 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright: Gabriel Marcano, 2023
 
+#include <adc.h>
+#include <syscalls.h>
+
 #include "am_mcu_apollo.h"
 #include "am_bsp.h"
 #include "am_util.h"
 
 #include <string.h>
 #include <assert.h>
-
-#include <uart.h>
-#include <adc.h>
-
-#define CHECK_ERRORS(x)               \
-	if ((x) != AM_HAL_STATUS_SUCCESS) \
-	{                                 \
-		error_handler(x);             \
-	}
-
-static void error_handler(uint32_t error)
-{
-	(void)error;
-	for(;;)
-	{
-		am_devices_led_on(am_bsp_psLEDs, 0);
-		am_util_delay_ms(500);
-		am_devices_led_off(am_bsp_psLEDs, 0);
-		am_util_delay_ms(500);
-	}
-}
-
-static struct uart uart;
-static struct adc adc;
+#include <stdint.h>
 
 int main(void)
 {
+	static struct adc adc;
+
 	// Prepare MCU by init-ing clock, cache, and power level operation
 	am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
 	am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
@@ -43,29 +25,30 @@ int main(void)
 	am_hal_sysctrl_fpu_stacking_enable(true);
 
 	// Init UART
-	uart_init(&uart, UART_INST0);
+	struct uart *uart = uart_get_instance(UART_INST0);
+	syscalls_uart_init(uart);
 
 	// Initialize the ADC.
-	adc_init(&adc);
+	static const uint8_t pins[] = { 16 };
+	adc_init(&adc, pins, 1);
 
 	// After init is done, enable interrupts
 	am_hal_interrupt_master_enable();
 
 	// Print the banner.
-	am_util_stdio_terminal_clear();
-	am_util_stdio_printf("Hello World!\r\n\r\n");
+	printf("Hello World!\r\n\r\n");
 
 	// Print the device info.
 	am_util_id_t device_id;
 	am_util_id_device(&device_id);
-	am_util_stdio_printf("Vendor Name: %s\r\n", device_id.pui8VendorName);
-	am_util_stdio_printf("Device type: %s\r\n", device_id.pui8DeviceName);
+	printf("Vendor Name: %s\r\n", device_id.pui8VendorName);
+	printf("Device type: %s\r\n", device_id.pui8DeviceName);
 
-	am_util_stdio_printf("Qualified: %s\r\n",
+	printf("Qualified: %s\r\n",
 						 device_id.sMcuCtrlDevice.ui32Qualified ?
 						 "Yes" : "No");
 
-	am_util_stdio_printf("Device Info:\r\n"
+	printf("Device Info:\r\n"
 						 "\tPart number: 0x%08X\r\n"
 						 "\tChip ID0:	0x%08X\r\n"
 						 "\tChip ID1:	0x%08X\r\n"
@@ -78,26 +61,24 @@ int main(void)
 
 	// If not a multiple of 1024 bytes, append a plus sign to the KB.
 	uint32_t mem_size = ( device_id.sMcuCtrlDevice.ui32FlashSize % 1024 ) ? '+' : 0;
-	am_util_stdio_printf("\tFlash size:  %7d (%d KB%s)\r\n",
+	printf("\tFlash size:  %7d (%d KB%s)\r\n",
 						 device_id.sMcuCtrlDevice.ui32FlashSize,
 						 device_id.sMcuCtrlDevice.ui32FlashSize / 1024,
 						 &mem_size);
 
 	mem_size = ( device_id.sMcuCtrlDevice.ui32SRAMSize % 1024 ) ? '+' : 0;
-	am_util_stdio_printf("\tSRAM size:   %7d (%d KB%s)\r\n\r\n",
+	printf("\tSRAM size:   %7d (%d KB%s)\r\n\r\n",
 						 device_id.sMcuCtrlDevice.ui32SRAMSize,
 						 device_id.sMcuCtrlDevice.ui32SRAMSize / 1024,
 						 &mem_size);
 
-	// Print the compiler version.
-	am_hal_uart_tx_flush(uart.handle);
-	am_util_stdio_printf("App Compiler:	%s\r\n", COMPILER_VERSION);
-	am_util_stdio_printf("HAL Compiler:	%s\r\n", g_ui8HALcompiler);
-	am_util_stdio_printf("HAL SDK version: %d.%d.%d\r\n",
+	printf("App Compiler:	%s\r\n", COMPILER_VERSION);
+	printf("HAL Compiler:	%s\r\n", g_ui8HALcompiler);
+	printf("HAL SDK version: %d.%d.%d\r\n",
 						 g_ui32HALversion.s.Major,
 						 g_ui32HALversion.s.Minor,
 						 g_ui32HALversion.s.Revision);
-	am_util_stdio_printf("HAL compiled with %s-style registers\r\n",
+	printf("HAL compiled with %s-style registers\r\n",
 						 g_ui32HALversion.s.bAMREGS ? "AM_REG" : "CMSIS");
 
 	am_hal_security_info_t security_info;
@@ -107,23 +88,22 @@ int main(void)
 		char string_buffer[32];
 		if (security_info.bInfo0Valid)
 		{
-			am_util_stdio_sprintf(string_buffer, "INFO0 valid, ver 0x%X", security_info.info0Version);
+			sprintf(string_buffer, "INFO0 valid, ver 0x%X", security_info.info0Version);
 		}
 		else
 		{
-			am_util_stdio_sprintf(string_buffer, "INFO0 invalid");
+			sprintf(string_buffer, "INFO0 invalid");
 		}
 
-		am_util_stdio_printf("SBL ver: 0x%x - 0x%x, %s\r\n",
+		printf("SBL ver: 0x%x - 0x%x, %s\r\n",
 			security_info.sblVersion, security_info.sblVersionAddInfo, string_buffer);
 	}
 	else
 	{
-		am_util_stdio_printf("am_hal_security_get_info failed 0x%X\r\n", status);
+		printf("am_hal_security_get_info failed 0x%X\r\n", status);
 	}
 
 	// Trigger the ADC to start collecting data
-	adc_trigger(&adc);
 
 	// Wait here for the ISR to grab a buffer of samples.
 	while (1)
@@ -131,25 +111,21 @@ int main(void)
 		// Print the battery voltage and temperature for each interrupt
 		//
 		uint32_t data = 0;
-		if (adc_get_sample(&adc, &data))
-		{
+		adc_trigger(&adc);
+		while (!(adc_get_sample(&adc, &data, pins, 1)));
+		// The math here is straight forward: we've asked the ADC to give
+		// us data in 14 bits (max value of 2^14 -1). We also specified the
+		// reference voltage to be 1.5V. A reading of 1.5V would be
+		// translated to the maximum value of 2^14-1. So we divide the
+		// value from the ADC by this maximum, and multiply it by the
+		// reference, which then gives us the actual voltage measured.
+		const double reference = 1.5;
+		double voltage = data * reference / ((1 << 14) - 1);
 
-			// The math here is straight forward: we've asked the ADC to give
-			// us data in 14 bits (max value of 2^14 -1). We also specified the
-			// reference voltage to be 1.5V. A reading of 1.5V would be
-			// translated to the maximum value of 2^14-1. So we divide the
-			// value from the ADC by this maximum, and multiply it by the
-			// reference, which then gives us the actual voltage measured.
-			const double reference = 1.5;
-			double voltage = data * reference / ((1 << 14) - 1);
+		printf(
+			"voltage = <%.3f> (0x%04X) ", voltage, data);
 
-			am_util_stdio_printf(
-				"voltage = <%.3f> (0x%04X) ", voltage, data);
-
-			am_util_stdio_printf("\r\n");
-		}
-
-		// Sleep here until the next ADC interrupt comes along.
-		am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_DEEP);
+		printf("\r\n");
+		am_util_delay_ms(100);
 	}
 }
